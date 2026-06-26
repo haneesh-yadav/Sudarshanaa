ac84eb9b4724e69cfe46ecc66e959bcef1ad8161
Haneesh Yadav <haneeshrao619@gmail.com>
2026-06-27 01:07:18 +0530

    updates

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
   },
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: 'jsdo',
     setupFiles: './src/test/setup.js',
     css: true,
   },
